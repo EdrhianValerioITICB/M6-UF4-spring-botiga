81485643c413ab9140882388155b4333c832f32e
Edrhian Valerio <edrhian27@gmail.com>
2024-05-28 21:01:51 +0200

    Updated for insertion

--- botiga/src/main/java/com/accesadades/botiga/Controller/WebController.java
+++ botiga/src/main/java/com/accesadades/botiga/Controller/WebController.java
@@ -27,18 +27,23 @@ public class WebController {
     @Autowired
     private SubcategoryService subcategoryService;
 
+    // Devuelve la pagina index
     @RequestMapping(value = "/")
     public String index(Model model) {
         return "index";
     }
 
+    // Devuelve la pagina del catalogo
     @RequestMapping(value = "/catalog")
     public String catalog(Model model) {
         Set<Product> products = productService.findAllProducts();
+        System.out.println(products);
         model.addAttribute("products", products);
         return "catalog";
     }
 
+    // Devuelve la pagina de busqueda, el nombre insertado dentro del cuadro tiene
+    // que ser EXACTO al buscado
     @RequestMapping(value = { "/search", "/prodname" }, method = { RequestMethod.GET, RequestMethod.POST })
     public String searchProductByName(@RequestParam(value = "name", required = false) String name, Model model) {
         if (name != null) {
@@ -48,27 +53,29 @@ public class WebController {
         return "search"; // Referencia a search.html en el directorio templates
     }
 
+    // Devuelve la pagina de insercion de un nuevo producto, con una lista de las
+    // subcategorias de la base de datos
     @RequestMapping(value = "/newproduct", method = { RequestMethod.GET })
     public String newProduct(Model model) {
         model.addAttribute("product", new Product());
         Set<Subcategory> subcategories = subcategoryService.findAllSubcategories();
-        for (Subcategory subcategory : subcategories) {
-            System.out.println(subcategory);
-        }
+
         model.addAttribute("subcategories", subcategories);
         return "newproduct";
     }
 
+    // Guarda el producto si no ha habido ningun error y redirige a la pagina del
+    // mensaje de confirmacion
+    // Si hay un error devuelve la pagina newproduct
     @RequestMapping(value = "/desar", method = { RequestMethod.POST })
     public String newProduct(@Validated @ModelAttribute("product") Product product, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "newproduct"; // Redirige de nuevo al formulario si hay errores
         }
-        
+
         LocalDateTime now = LocalDateTime.now();
         product.setCreationDate(now);
         product.setUpdateDate(now);
-        System.out.println(product);
         productService.saveProduct(product);
         model.addAttribute("message", "Producte inserit correctament");
         return "success_prod_created";
